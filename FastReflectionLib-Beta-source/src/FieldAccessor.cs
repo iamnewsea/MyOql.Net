77b29a3bfdd9e2fa5474de253fbe2aa93afceaa7
于新海 <iamnewsea@163.com>
2015-05-22 11:52:51 +0800

    .1

---
+++ FastReflectionLib-Beta-source/src/FieldAccessor.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Reflection;
+using System.Linq.Expressions;
+
+namespace FastReflectionLib
+{
+    public interface IFieldAccessor
+    {
+        object GetValue(object instance);
+        void SetValue(object instance, object value);
+    }
+
+    public class FieldAccessor : IFieldAccessor
+    {
+        private Func<object, object> m_getter;
+
+        public FieldInfo FieldInfo { get; private set; }
+
+        public FieldAccessor(FieldInfo fieldInfo)
+        {
+            this.FieldInfo = fieldInfo;
+            InitializeGet(fieldInfo);
+            InitializeSet(fieldInfo);
+        }
+
+        private void InitializeGet(FieldInfo fieldInfo)
+        {
+            // target: (object)(((TInstance)instance).Field)
+
+            // preparing parameter, object type
+            var instance = Expression.Parameter(typeof(object), "instance");
+
+            // non-instance for static method, or ((TInstance)instance)
+            var instanceCast = fieldInfo.IsStatic ? null :
+                Expression.Convert(instance, fieldInfo.ReflectedType);
+
+            // ((TInstance)instance).Property
+            var fieldAccess = Expression.Field(instanceCast, fieldInfo);
+
+            // (object)(((TInstance)instance).Property)
+            var castFieldValue = Expression.Convert(fieldAccess, typeof(object));
+
+            // Lambda expression
+            var lambda = Expression.Lambda<Func<object, object>>(castFieldValue, instance);
+
+            this.m_getter = lambda.Compile();
+        }
+
+        private Action<object, object> m_setter;
+
+        private void InitializeSet(FieldInfo fieldInfo)
+        {
+            // target: (((TInstance)instance).Field) = value;
+
+            // preparing parameter, object type
+            var instance = Expression.Parameter(typeof(object), "instance");
+            var value = Expression.Parameter(typeof(object), "value");
+
+            // non-instance for static method, or ((TInstance)instance)
+            var instanceCast = fieldInfo.IsStatic ? null :
+                Expression.Convert(instance, fieldInfo.ReflectedType);
+
+            // ((TInstance)instance).Property
+            var fieldAccess = Expression.Field(instanceCast, fieldInfo);
+
+            // (object)(((TInstance)instance).Property)
+            var castFieldValue = Expression.Assign(fieldAccess, Expression.Convert(value, fieldInfo.FieldType));
+
+            // Lambda expression
+            var lambda = Expression.Lambda<Action<object, object>>(castFieldValue, instance, value);
+
+            this.m_setter = lambda.Compile();
+        }
+
+        public object GetValue(object instance)
+        {
+            return this.m_getter(instance);
+        }
+
+        public void SetValue(object instance, object value)
+        {
+            this.m_setter(instance, value);
+        }
+
+        #region IFieldAccessor Members
+
+        object IFieldAccessor.GetValue(object instance)
+        {
+            return this.GetValue(instance);
+        }
+
+        void IFieldAccessor.SetValue(object instance, object value)
+        {
+            this.SetValue(instance, value);
+        }
+        #endregion
+    }
+}
